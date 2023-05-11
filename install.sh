a21cefc939f81bf6f7454b2710e676fa5bdbe572
Mahantesh S Meti <mann@Mahanteshs-MacBook-Pro.local>
2023-05-11 15:01:48 +0530

    Initial ver

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 helm lint charts
 echo -e "User-Agent: *\nDisallow: /" > robots.txt 
-helm package ./
+helm package ./charts
 helm repo index --url https://github.kyndryl.net/rhocp41test1/mscp-rancher-templates.git .
 cat index.yaml
 git add .
