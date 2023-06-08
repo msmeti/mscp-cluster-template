ec943182c569d1711454e63258429418665e717e
Mahantesh S Meti <mann@Mahanteshs-MacBook-Pro.local>
2023-06-08 19:42:41 +0530

    Initial ver

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 helm lint charts/rancher-aws-cluster-template
 echo -e "User-Agent: *\nDisallow: /" > robots.txt 
 helm package charts/rancher-aws-cluster-template
-helm repo index --url https://github.kyndryl.net/rhocp41test1/mscp-rancher-templates.git .
+helm repo index --url https://github.com/msmeti/mscp-cluster-template.git .
 cat index.yaml
 git add .
 git commit -m "Initial ver"
